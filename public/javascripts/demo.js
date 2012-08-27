217dbf85592b62b91e20ae05c480a092e17cfba0
Svein Melby <sme@forskningsradet.no>
2012-08-27 11:39:18 +0200

    Legger til eksempler i demo.js

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -1,5 +1,82 @@
 ﻿$(function () {
     "use strict";
+    // Funksjoner
+    function summer(a, b) {
+    	var sum = a + b; 
+    	return sum;
+    }
+    
+   
+    
+    //console.log(
+    (function(a, b){
+    	return a + b;
+    })(2, 5)
+    //);
+    
+    // ...Nestet funksjon
+    function hei(navn) {
+    	function skrivUt() {
+    		console.log("HEI " + navn);
+    	}
+    	skrivUt();
+    }
+    
+    // hei("Per");
+    
+    // .. Kan tilordnes variable
+    var summer2 = function(a, b){
+    	return a + b;
+    };
+    
+    
+    // ...Kan sendes som parametere
+  	function logger(noe) {
+    	console.log("LOGG: " + noe);
+    };
+	
+    function summer3(a, b, loggFn) {
+    	var sum = a + b;
+	    loggFn(sum);
+	};
+	// console.log( summer3(2, 4, logger));
+	
+	// ...Kan ha properties og funksjoner
+    summer4.sum = 0;
+    summer4.skrivSum = function(tall) {
+       	   console.log("Sum: " + tall);
+  	};
+
+   	function summer4(a, b) {
+   	 summer4.sum = a + b; 
+   	 summer4.skrivSum(summer4.sum);
+   	}
+   	
+   	// summer4(4, 5);
+   	// console.log(summer4.skrivSum);
+
+
+   	// Scope
+   	function testScope() {
+        // Denne blir nå tilgjengeliggjort i globalt scope
+        globalVariabel = "global";
+
+        // Denne er bare tilgjengelig i funksjonen
+        var lokalVariabel = "lokal";
+
+        // Lager variabelen i i funksjonens scope, ikke løkkens scope
+        for (var i = 0; i < 2; i++) {
+          console.log(i);
+        }
+
+        // Variabelen i er allerede deklarert - kan gi uønskede effekter
+        for (var i = 0; i < 2; i++) {
+           console.log(i);
+        }
+      }
+
+    
+    
 
     // Objekter
     var person = {
